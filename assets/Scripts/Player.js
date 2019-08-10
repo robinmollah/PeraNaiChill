9496aa6fabee1c2947abeec3d43542e50f4dc963
robinmollah <robinsajin@gmail.com>
2019-08-10 20:51:38 +0600

    ScoreLabel added

--- assets/Scripts/Player.js
+++ assets/Scripts/Player.js
@@ -3,6 +3,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+
     },
 
 
@@ -11,11 +12,16 @@ cc.Class({
         manager.enabled = true;
         manager.enabledDebugDraw = true;
         manager.enabledDrawBoundingBox = true;
+        this.score = 0;
     },
 
     onCollisionEnter(other, self){
         if(other.getComponent('Pera').pickedPera.func){
             other.getComponent('Pera').pickedPera.func();
+        } else if(other.getComponent('Pera').pickedPera.score){
+            this.score += other.getComponent('Pera').pickedPera.score;
+            let scoreLabel = cc.find('/Canvas/ScoreLabel').getComponent(cc.Label);
+            scoreLabel.string = "Score: " + this.score;
         }
         other.getComponent('Pera').init();
     },
