de07f8ddb71302f7e92dafccdae6fedb14605b6b
robinmollah <robinsajin@gmail.com>
2019-09-07 17:03:41 +0600

    MainScene animation added

--- assets/Scripts/MainSceneScript.js
+++ assets/Scripts/MainSceneScript.js
@@ -11,7 +11,11 @@ cc.Class({
     // onLoad () {},
 
     start () {
-
+        let logo = cc.find('/Canvas/Pera-Nai-Chill').getComponent(cc.Animation);
+        logo.play();
+        logo.on(cc.Animation.EventType.FINISHED, function(){
+            logo.play();
+        });
     },
 
     // update (dt) {},
